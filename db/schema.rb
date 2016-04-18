096c593aa8e6ed178ce995ab78d4d045a02f7e42
Muneki Aso <muneki-aso@united.jp>
2016-04-18 05:08:45 +0000

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160414071347) do
+ActiveRecord::Schema.define(version: 20160418025229) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
